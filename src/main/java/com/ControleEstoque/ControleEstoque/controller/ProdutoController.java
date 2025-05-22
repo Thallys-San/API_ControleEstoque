394934631a9080248f00fe65f4af9d48a34846e5
Thallys_San <ricardo.viasolo142@gmail.com>
2025-05-22 16:43:48 -0300

    Update ProdutoController.java

--- src/main/java/com/ControleEstoque/ControleEstoque/controller/ProdutoController.java
+++ src/main/java/com/ControleEstoque/ControleEstoque/controller/ProdutoController.java
@@ -1,5 +1,32 @@
 package com.ControleEstoque.ControleEstoque.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ControleEstoque.ControleEstoque.model.Produto;
+
+
+
+@RestController
+
+@RequestMapping("/api/produto")
 public class ProdutoController {
     
+    @Autowired
+    private Produto service;
+    
+    public List<Produto> listarTodos(){
+        return service.listarTodos();
+    }
+
+    public ResponseEntity<Produto> buscarPorId(@PathVariable Long id){
+        return service.buscarPorId(id)
+        
+    }
+
 }
