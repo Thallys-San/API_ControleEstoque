30600eb50759906f95ae562d2b6e74f756e0187c
Thallys_San <ricardo.viasolo142@gmail.com>
2025-05-22 16:44:05 -0300

    Update ProdutoRepository.java

--- src/main/java/com/ControleEstoque/ControleEstoque/repository/ProdutoRepository.java
+++ src/main/java/com/ControleEstoque/ControleEstoque/repository/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package com.ControleEstoque.ControleEstoque.repository;
 
-public class ProdutoRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ControleEstoque.ControleEstoque.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
 }
